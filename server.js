91d6dda85b53fc0dc807095f26651ce0fbb35e1c
Joshua M. Bedilion <jbedilio@yahoo.com>
2017-10-19 00:45:20 -0400

    building server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //requiring dependencies
 var express = require('express');
 var bp = require('body-parser');
+var logger = require('morgan');
 var request = require('request');
 var cheerio = require('cheerio');
 var mongoose = require('mongoose');
@@ -8,6 +9,11 @@ var mongoose = require('mongoose');
 //setting a port
 var PORT = process.env.PORT || 3000;
 
+//setting Mongoose to leverage built in JS ES6 Promises
+mongoose.Promise = Promise;
+
+//require the model
+//var Article = require('./model.js');
 
 //grabbing an instance of express
 var app = express();
@@ -15,8 +21,14 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
+//setting the instance of express to use morgan & body-parser
+app.use(logger('dev'));
 app.use(bp.urlencoded({extended: true}));
 app.use(bp.json());
+
+//database config for mongoose and setting it to the var db
+mongoose.connect('mongodb://localhost/newsScraperdb');
+var db = mongoose.connection;
 
 //set handlebars
 var exphbs = require('express-handlebars');
